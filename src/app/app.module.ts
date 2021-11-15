550a44ed6f58002da311767ea2ba334c33c3432c
DatskoRoman <pbk.datsko@gmail.com>
2021-11-15 16:56:24 +0200

    HW post відобразити всі пости.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 // @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    UsersComponent
+    PostComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
