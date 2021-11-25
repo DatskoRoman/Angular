eac5c6087486cc89262d6e821e321d1bd4f80633
DatskoRoman <pbk.datsko@gmail.com>
2021-11-25 20:02:32 +0200

    lesson 5 HW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,61 +2,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { PostInfoComponent } from './components/post-info/post-info.component';
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersFormComponent } from './components/users-form/users-form.component';
-import { UsersComponent } from './components/users/users.component';
 import { StringTypePipe } from './pipes/string-type.pipe';
-import { UserResolveService } from './services/user-resolve.service';
+import { HomeComponent } from './components/home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    UserInfoComponent,
-    UsersFormComponent,
-    PostComponent,
-    PostsComponent,
-    PostInfoComponent,
-    CommentComponent,
-    CommentsComponent,
-    StringTypePipe
+    StringTypePipe,
+    HomeComponent
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'users',
-        component: UsersComponent,
-        children: [
-          {
-            path: ':id',
-            component: UserInfoComponent,
-            resolve: { data: UserResolveService }
-          }
-        ]
-      },
-      {path: "posts",
-        component: PostsComponent,
-        children:[
-          {
-            path: ":id",
-            component: PostInfoComponent
-          },
-        ]},
-      { path: 'comments', component: CommentsComponent },
-      { path: 'users-select', component: UsersFormComponent }
-    ]),
+    AppRoutingModule,
     ReactiveFormsModule
   ],
   providers: [],
